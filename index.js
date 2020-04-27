cfae0602346fe82f8223a074439d9e45a9486dc6
igopib <gs109690@gmail.com>
2020-04-27 02:44:37 -0400

    calling and making functions

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ person[selection] = 'Gopiinho';
 
 */
 
+/*   Arrays 
 let selectedColours = ['red', 'green'];
  selectedColours[2] = 1
 console.log(selectedColours);
+
+*/
+
+// function to perform a task
+
+function greet(name, lastName) {
+    console.log('Hello ' +  name + ' ' + lastName);
+}
+
+// function to calculate a value
+function square(number) {
+    return number * number;
+}
+
+//let number = square(2);
+console.log(square(2));
+
+greet('Gopiinho', 'Singh');
